da4798bb26d56b05fb99ec379fee1b56b1f50c97
Damian Zborowski <damian.zboro@gmail.com>
2021-05-09 17:58:44 +0200

    c

--- src/app/game/view/Game.tsx
+++ src/app/game/view/Game.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
-import {IGame} from "../interface/IGame";
 
-interface IProps {
-    game: IGame;
-}
-
-export class Game extends React.Component<IProps> {
+export class Game extends React.Component {
     public render() {
         return <div className={"Game"}></div>;
     }
