84ceffb161016d6e7c52e1566120e0c592fae4c7
Damian Zborowski <damian.zboro@gmail.com>
2021-05-09 18:48:05 +0200

    c

--- src/app/game/view/GamesList.tsx
+++ src/app/game/view/GamesList.tsx
@@ -1,25 +1,26 @@
 import * as React from "react";
-import {rootStore, socket} from "../../../renderer";
+import {rootStore} from "../../../renderer";
 import {observer} from "mobx-react";
 import {Button} from "react-bootstrap";
-import {IGame} from "../interface/IGame";
 
 @observer
 export class GamesList extends React.Component {
-    public async componentDidMount() {
-        await rootStore.game.loadGames();
-        socket.on("new-game-created", () => {
-            rootStore.game.loadGames();
-        });
+    public componentDidMount() {
+        rootStore.game.loadGames();
+        // socket.on("new-game-created", () => {
+        //     rootStore.game.loadGames();
+        // });
     }
 
     protected createGame = () => {
+        rootStore.game.loadGames();
         // socket.emit("createGame");
     };
 
     protected joinToGame = (gameId: string) => {};
 
     public render() {
+        console.log("rootStore.game.games:", rootStore.game.games);
         return (
             <div className={"GamesList"}>
                 <Button variant="primary" onClick={this.createGame}>
@@ -27,14 +28,15 @@ export class GamesList extends React.Component {
                 </Button>
 
                 <div className={"available-games"}>
-                    {rootStore.game.games.map((game: IGame) => (
-                        <div key={game.id} className={"available-game"}>
-                            <div>{game.id}</div>
-                            <Button variant="success" onClick={() => this.joinToGame(game.id)}>
-                                Join to game
-                            </Button>
-                        </div>
-                    ))}
+                    {rootStore.game.games[0]?.id}
+                    {/* {rootStore.game.games.map((game: IGame) => (*/}
+                    {/*    <div key={game.id} className={"available-game"}>*/}
+                    {/*         <div>{game.id}</div>*/}
+                    {/*         <Button variant="success" onClick={() => this.joinToGame(game.id)}>*/}
+                    {/*            Join to game*/}
+                    {/*         </Button>*/}
+                    {/*    </div>*/}
+                    {/* ))}*/}
                 </div>
             </div>
         );
